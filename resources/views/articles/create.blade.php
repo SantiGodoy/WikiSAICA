aa02ecedf93b9032f84c65305f1f7785197a18e4
Samuel-Ambrosio <44056254+Samuel-Ambrosio@users.noreply.github.com>
2019-04-04 19:04:00 +0200

    Ordenando un poco
    He creado una carpeta llamada 'partials' en la carpeta views, dónde estarán el nav, footer, el topnav, etc...

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,76 +1,79 @@
 @extends('layout')
-
-<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
-<script src="https://cdn.ckeditor.com/4.11.3/standard-all/ckeditor.js"></script>
-
-<div class="d-flex" id="wrapper">
-<div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading"><center><img  width = 170px src="{{ URL::asset('img/saica.jpg') }}"></center></div>
-      <div class="list-group list-group-flush">
-        <a href="{{route('articles.index')}}" class="list-group-item list-group-item-action bg-light">Home</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
-      </div>
-    </div>
-<style>
-  .uper {
-    margin-top: 40px;
-  }
-</style>
-@section('content')
-<div class="card uper">
-  <div class="card-header">
-    Add Article
-  </div>
-  <div class="card-body">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-      </div><br />
-    @endif
-    <div class="container-fluid">
-      <form method="post" action="{{ route('articles.store') }}">
-        <div class="form-group">
-              @csrf
-              <label for="name">Article Title:</label>
-              <input type="text" class="form-control" name="article_title"/>
-        </div>
-          <div class="form-group">
-              <label for="price">Article Description :</label>
-              <textarea style="width: 100%;" id ="article_description" name="article_description"></textarea><br/>
-              <script>
-                CKEDITOR.replace('article_description', {
-                fullPage: false,
-                extraPlugins: 'docprops',
-              // Disable content filtering because if you use full page mode, you probably
-              // want to  freely enter any HTML content in source mode without any limitations.
-                allowedContent: true,
-                height: 350
-              });
-          </script>
-          <script>  
-              function getData() {  
-              //Get data written in first Editor   
-              var editor_data = CKEDITOR.instances['article_description'].getData();  
-              //Set data in Second Editor which is written in first Editor  
-              document.getElementById("description").innerHTML = editor_data; 
-            }  
-          </script> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+        <script src="https://cdn.ckeditor.com/4.11.3/standard-all/ckeditor.js"></script>
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                @include('partials.nav')
+            </div>
             
-          </div>
-          <br>
+            <style>
+                .uper {
+                    margin-top: 40px;
+                }
+            </style>
 
-          <button type="submit" class="btn btn-primary">Add</button>
-      </form>
-    </div>
-  </div>
-</div>
-</div>
-@endsection
+            @section('content')
+            <div class="card uper">
+                <div class="card-header">
+                    Add Article
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <br>
+                    @endif
+                    <div class="container-fluid">
+                        <form method="post" action="{{ route('articles.store') }}">
+                            <div class="form-group">
+                                @csrf
+                                <label for="name">Article Title:</label>
+                                <input type="text" class="form-control" name="article_title"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="price">Article Description :</label>
+                                <textarea style="width: 100%;" id ="article_description" name="article_description"></textarea><br/>
+                                <script>
+                                    CKEDITOR.replace('article_description', {
+                                    fullPage: false,
+                                    extraPlugins: 'docprops',
+                                    // Disable content filtering because if you use full page mode, you probably
+                                    // want to  freely enter any HTML content in source mode without any limitations.
+                                    allowedContent: true,
+                                    height: 350
+                                    });
+                                </script>
+                                <script>  
+                                    function getData() {  
+                                    //Get data written in first Editor   
+                                    var editor_data = CKEDITOR.instances['article_description'].getData();  
+                                    //Set data in Second Editor which is written in first Editor  
+                                    document.getElementById("description").innerHTML = editor_data; 
+                                }  
+                                </script>
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary">Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endsection
+    </body>
+</html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="sidebar-heading"><center><a href="{{url('/')}}"><img width = 170px src="{{ URL::asset('img/saica.jpg') }}"></a></center></div>
+<div class="list-group list-group-flush">
+    <a href="{{route('articles.index')}}" class="list-group-item list-group-item-action bg-light">Artículos</a>
+    <a href="{{route('articles.create')}}" class="list-group-item list-group-item-action bg-light">Crear artículo</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+</div>
