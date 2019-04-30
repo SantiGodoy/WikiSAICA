804a9f21824153ffeb6d0ef89a89bc6270ed3f6e
fannyvd97 <33985674+fannyvd97@users.noreply.github.com>
2019-04-30 16:58:29 +0200

    Corrección en el mostrar artículo

--- resources/views/articles/show_article.blade.php
+++ resources/views/articles/show_article.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
         <script src="https://cdn.ckeditor.com/4.11.3/standard-all/ckeditor.js"></script>
@@ -15,7 +15,7 @@
             <div class="bg-light border-right" id="sidebar-wrapper">
                 @include('partials.nav')
             </div>
-            
+
             <style>
                 .uper {
                     margin-top: 40px;
@@ -29,7 +29,7 @@
                         <a href="{{ route('articles.edit',$article->id)}}">Edit</a>
                     </div>
                     <br>
-                    
+
                     <div class = "column" style="float: right; width: 30%">
                         <table class="table table-bordered">
                           <tbody>
@@ -52,22 +52,22 @@
                           </tbody>
                         </table>
                     </div>
-                     
+
                          <div id={{$article->id}}>{{$article->description}}>
                         <script type="text/javascript">
                             var id = @json($article->id);
-                            document.getElementById(id).innerHTML = @json($article->description); 
+                            document.getElementById(id).innerHTML = @json($article->description);
                         </script>
 
                         </div>
                         <br>
-                     <div style="text-align: right; right: 5%; position: absolute; bottom: 5%;">
+                     <div style="text-align: right; right: 5%; position: absolute; width: 100% bottom: 5%; margin-bottom: 3%">
                             <i>Last modified: {{$article->updated_at}}</i>
                     </div>
                 </div>
 
             </div>
-            
+
         </div>
     </body>
 </html>
