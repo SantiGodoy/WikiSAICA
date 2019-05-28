5235c1d85657e1331f098ad29dc6e4b88a862016
Samuel-Ambrosio <44056254+Samuel-Ambrosio@users.noreply.github.com>
2019-05-28 13:46:07 +0200

    Pequeño cambio

--- resources/views/user/articles_by_user.blade.php
+++ resources/views/user/articles_by_user.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Home</title>
+        <title>Mis artículos</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
